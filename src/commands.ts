c9a204c0914c3ba2d3759f9ccaf19ffc6c8d1569
Michał Miłek <michal.mileq@gmail.com>
2024-06-21 10:24:01 +0200

    Queue command

--- src/commands.ts
+++ src/commands.ts
@@ -2,11 +2,13 @@ import NicheBotCommand from "./NicheBotCommand";
 import joinCommand from "./music/join";
 import leaveCommand from "./music/leave";
 import playCommand from "./music/play";
+import queueCommand from "./music/queue";
 import pingCommand from "./tests/ping";
 
 export default [
   pingCommand,
   playCommand,
   joinCommand,
-  leaveCommand
+  leaveCommand,
+  queueCommand
 ] as NicheBotCommand[];
---
+++ src/music/queue.ts
@@ -0,0 +1,28 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import BOT_STATE from "../BotState";
+import NicheBotCommand from "../NicheBotCommand";
+
+const data = new SlashCommandBuilder()
+  .setName("queue")
+  .setDescription("Show the current queue");
+
+async function execute(interaction: ChatInputCommandInteraction) {
+  if (!BOT_STATE.voiceConnection) {
+    await interaction.reply("I'm not in a voice channel!");
+    return;
+  }
+
+  if (BOT_STATE.songQueue.length === 0) {
+    await interaction.reply("The queue is empty!");
+    return;
+  }
+
+  const reply = BOT_STATE.songQueue
+    .map((v, i) => `${i + 1}. ${v.title}`)
+    .join("\n");
+
+  interaction.reply("Current queue:\n" + reply);
+}
+
+const queueCommand = new NicheBotCommand(data, execute);
+export default queueCommand;
